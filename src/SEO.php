5fd02c00e5de2a16f30d5fc55ddcca4fd846569e
Sean O'Donnell <sean.odonnell@digitonic.co.uk>
2025-08-26 09:17:35 +0100

    FSEO-1 Various updates for Filament V4

--- src/SEO.php
+++ src/SEO.php
@@ -2,10 +2,10 @@
 
 namespace RalphJSmit\Filament\SEO;
 
-use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
